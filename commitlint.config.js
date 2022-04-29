b21e21c90523394566800ae05dca8df9ef93c7e7
zhuohualuo <2471036962@qq.com>
2022-04-29 19:26:03 +0800

    foo: this will fail

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 108],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'subject-case': [0],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feature',
+        'fix',
+        'refactor',
+        'docs',
+        'perf',
+        'model',
+        'data',
+        'prod'
+      ]
+    ]
+  }
 };
