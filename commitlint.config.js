2ecee4f72554b8f8d64882e978e7a278c809bff0
zhuohualuo <2471036962@qq.com>
2022-05-01 09:38:28 +0800

    feat: 222

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  // commit testK
+  // commit test
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'body-leading-blank': [2, 'always'],
-    'footer-leading-blank': [1, 'always'],
-    'header-max-length': [2, 'always', 108],
-    'subject-empty': [2, 'never'],
-    'type-empty': [2, 'never'],
-    'subject-case': [0],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feature',
-        'fix',
-        'refactor',
-        'docs',
-        'perf',
-        'model',
-        'data',
-        'prod'
-      ]
-    ]
-  }
+  // rules: {
+  //   'body-leading-blank': [2, 'always'],
+  //   'footer-leading-blank': [1, 'always'],
+  //   'header-max-length': [2, 'always', 108],
+  //   'subject-empty': [2, 'never'],
+  //   'type-empty': [2, 'never'],
+  //   'subject-case': [0],
+  //   'type-enum': [
+  //     2,
+  //     'always',
+  //     [
+  //       'feature',
+  //       'fix',
+  //       'refactor',
+  //       'docs',
+  //       'perf',
+  //       'model',
+  //       'data',
+  //       'prod'
+  //     ]
+  //   ]
+  // }
 };
